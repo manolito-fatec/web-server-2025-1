6b6bc3f86c86225c586b41ac7b231e7686eb486f
André Wakugawa <andre.wakugawa@outlook.com>
2025-03-07 22:55:04 -0300

    🎉feat: initial project files
    initial dependencies
    +spring boot starter batch
    +spring boot starter data jdbc
    +spring boot starter data jpa
    +spring boot starter jdbc
    +spring boot starter web
    +flyway core
    +flyway database postgresql
    +postgresql
    +spring boot starter test
    +spring batch test
    +spring security test
    +lombok
    
    -spring security dependency commented out until auth is implemented.

---
+++ src/test/java/com/manolito/dashflow/DashflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manolito.dashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
