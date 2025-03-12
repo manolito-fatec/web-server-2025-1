7c3de7ebeb68e4e6314ccfe4eb809f75a9700538
AndreWakugawa <andre.wakugawa@outlook.com>
2025-03-12 19:24:29 -0300

    🚨(fix): taiga enum implementation

--- src/main/java/com/manolito/dashflow/enums/TaigaEndpoints.java
+++ src/main/java/com/manolito/dashflow/enums/TaigaEndpoints.java
@@ -6,55 +6,23 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum TaigaEndpoints implements EndpointService {
+public enum TaigaEndpoints{
 
-    PROJECTS("/api/v1/projects") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    PROJECTS("/api/v1/projects"),
 
-    USER_STORIES("/api/v1/userstories") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    USER_STORIES("/api/v1/userstories"),
 
-    TASKS("/api/v1/tasks") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    TASKS("/api/v1/tasks"),
 
-    ISSUES("/api/v1/issues") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    ISSUES("/api/v1/issues"),
 
-    USER_STORY_STATUSES("/api/v1/userstory-statuses") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    USER_STORY_STATUSES("/api/v1/userstory-statuses"),
 
-    EPICS("/api/v1/epics") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    EPICS("/api/v1/epics"),
 
-    ROLES("/api/v1/roles") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    },
+    ROLES("/api/v1/roles"),
 
-    PROJECT_MEMBERS("/api/v1/projects/{project_id}/members") {
-        @Override
-        public void handleRequest(String requestUrl) {
-        }
-    };
+    PROJECT_MEMBERS("/api/v1/projects/{project_id}/members");
 
     private final String path;
 }
