326917a7bc8eb99df8e24c3240d6864cd89c62a3
AndreWakugawa <andre.wakugawa@outlook.com>
2025-03-12 19:22:57 -0300

    ✨(feat): identifyTool + trello and clickup api url

--- src/main/java/com/manolito/dashflow/enums/ProjectManagementTool.java
+++ src/main/java/com/manolito/dashflow/enums/ProjectManagementTool.java
@@ -7,15 +7,25 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ProjectManagementTool {
 
-    TAIGA("https://api.taiga.io", TaigaEndpoints.class);
+    TAIGA("https://api.taiga.io"),
+    TRELLO("https://api.trello.com"),
+    CLICKUP("https://api.clickup.com/api");
 
     private final String baseUrl;
-    private final Class<? extends Enum<?>> endpointsClass;
 
     public String getFullUrl(Enum<?> endpoint) {
         if (endpoint instanceof TaigaEndpoints taigaEndpoint) {
             return this.baseUrl + taigaEndpoint.getPath();
         }
         throw new IllegalArgumentException("Endpoint não suportado: " + endpoint);
+    }
+
+    public static ProjectManagementTool identifyTool(String requestUrl) {
+        for (ProjectManagementTool tool : values()) {
+            if (requestUrl.contains(tool.getBaseUrl())) {
+                return tool;
+            }
+        }
+        return null;
     }
 }
