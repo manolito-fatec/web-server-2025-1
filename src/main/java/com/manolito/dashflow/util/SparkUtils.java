7623b444f30baa1395c2d7dce6c5f5b746898c7c
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-13 17:19:59 -0300

    ✨(feat): added auth post to get token

--- src/main/java/com/manolito/dashflow/util/SparkUtils.java
+++ src/main/java/com/manolito/dashflow/util/SparkUtils.java
@@ -1,14 +1,24 @@
 package com.manolito.dashflow.util;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.manolito.dashflow.dto.dw.AuthDTO;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+
 @Component
 public class SparkUtils {
+
+    private static final String API_URL = "https://api.taiga.io/api/v1/auth";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public String fetchDataFromEndpoint(String url) {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -25,6 +35,40 @@ public class SparkUtils {
             return EntityUtils.toString(response.getEntity());
         } catch (Exception e) {
             throw new RuntimeException("Error fetching data from endpoint: " + url, e);
+        }
+    }
+
+    public String authenticateTaiga(String username, String password) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost post = new HttpPost(API_URL);
+
+            post.addHeader("Accept", "application/json");
+            post.addHeader("Content-Type", "application/json");
+
+            AuthDTO authRequest = AuthDTO.builder()
+                    .username(username)
+                    .password(password)
+                    .type("normal")
+                    .build();
+
+            String jsonBody = objectMapper.writeValueAsString(authRequest);
+
+            StringEntity entity = new StringEntity(jsonBody, StandardCharsets.UTF_8);
+            post.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(post);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Falha na autenticação: " + response.getStatusLine().getStatusCode());
+            }
+
+            String responseString = EntityUtils.toString(response.getEntity());
+            JsonNode jsonNode = objectMapper.readTree(responseString);
+
+            return jsonNode.get("auth_token").asText();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao autenticar no Taiga", e);
         }
     }
 }
