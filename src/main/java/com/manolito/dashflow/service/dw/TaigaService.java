6a303ec4d2092a72ec577bf4aacdb53be8e18849
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2025-03-13 14:02:48 -0300

    ✨(feat): add basic spark handling on taiga service

--- src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
+++ src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
@@ -1,23 +1,43 @@
 package com.manolito.dashflow.service.dw;
 
+import com.manolito.dashflow.enums.ProjectManagementTool;
+import com.manolito.dashflow.enums.TaigaEndpoints;
+import com.manolito.dashflow.util.SparkUtils;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static com.manolito.dashflow.enums.ProjectManagementTool.TAIGA;
+import static com.manolito.dashflow.enums.TaigaEndpoints.*;
+
 @Service
+@RequiredArgsConstructor
 public class TaigaService {
 
-    public void handleProjects() {
-        System.out.println("Handling Taiga projects...");
-        // Add logic to handle projects
+    private final SparkSession spark;
+    private final SparkUtils utils;
+
+    public Dataset<Row> handleProjects() {
+        return fetchDataAsDataFrame(TAIGA.name() + PROJECTS.name());
     }
 
-    public void handleUserStories() {
-        System.out.println("Handling Taiga user stories...");
-        // Add logic to handle user stories
+    public Dataset<Row> handleUserStories() {
+        return fetchDataAsDataFrame(TAIGA.name() + USER_STORIES.name());
     }
 
-    public void handleTasks() {
-        System.out.println("Handling Taiga tasks...");
-        // Add logic to handle tasks
+    public Dataset<Row> handleTasks() {
+        return fetchDataAsDataFrame(TAIGA.name() + TASKS.name());
+    }
+
+    private Dataset<Row> fetchDataAsDataFrame(String url) {
+        String jsonResponse = utils.fetchDataFromEndpoint(url);
+        return spark.read().json(spark.createDataset(List.of(jsonResponse), Encoders.STRING()));
     }
 
 }
