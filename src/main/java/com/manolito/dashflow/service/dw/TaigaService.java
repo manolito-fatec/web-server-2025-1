646cce3858f3cf49f397523fba23593acf13794b
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-13 21:01:47 -0300

    🐛(fix): Moved code from SparkUtils to TaigaService for better folder organization

--- src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
+++ src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
@@ -1,7 +1,16 @@
 package com.manolito.dashflow.service.dw;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.manolito.dashflow.dto.dw.AuthDTO;
 import com.manolito.dashflow.util.SparkUtils;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -9,6 +18,7 @@ import org.apache.spark.sql.SparkSession;
 
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static com.manolito.dashflow.enums.ProjectManagementTool.TAIGA;
@@ -20,6 +30,8 @@ public class TaigaService {
 
     private final SparkSession spark;
     private final SparkUtils utils;
+    private static final String API_URL = "https://api.taiga.io/api/v1/auth";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public Dataset<Row> handleProjects() {
         return fetchDataAsDataFrame(TAIGA.name() + PROJECTS.name());
@@ -56,6 +68,40 @@ public class TaigaService {
     private Dataset<Row> fetchDataAsDataFrame(String url) {
         String jsonResponse = utils.fetchDataFromEndpoint(url);
         return spark.read().json(spark.createDataset(List.of(jsonResponse), Encoders.STRING()));
+    }
+
+    public String authenticateTaiga(String username, String password) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost post = new HttpPost(API_URL);
+
+            post.addHeader("Accept", "application/json");
+            post.addHeader("Content-Type", "application/json");
+
+            AuthDTO authRequest = AuthDTO.builder()
+                    .username(username)
+                    .password(password)
+                    .type("normal")
+                    .build();
+
+            String jsonBody = objectMapper.writeValueAsString(authRequest);
+
+            StringEntity entity = new StringEntity(jsonBody, StandardCharsets.UTF_8);
+            post.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(post);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Falha na autenticação: " + response.getStatusLine().getStatusCode());
+            }
+
+            String responseString = EntityUtils.toString(response.getEntity());
+            JsonNode jsonNode = objectMapper.readTree(responseString);
+
+            return jsonNode.get("auth_token").asText();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao autenticar no Taiga", e);
+        }
     }
 
 }
--- src/main/java/com/manolito/dashflow/util/SparkUtils.java
+++ src/main/java/com/manolito/dashflow/util/SparkUtils.java
@@ -1,12 +1,8 @@
 package com.manolito.dashflow.util;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.manolito.dashflow.dto.dw.AuthDTO;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -16,9 +12,6 @@ import java.nio.charset.StandardCharsets;
 
 @Component
 public class SparkUtils {
-
-    private static final String API_URL = "https://api.taiga.io/api/v1/auth";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public String fetchDataFromEndpoint(String url) {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
@@ -38,37 +31,4 @@ public class SparkUtils {
         }
     }
 
-    public String authenticateTaiga(String username, String password) {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpPost post = new HttpPost(API_URL);
-
-            post.addHeader("Accept", "application/json");
-            post.addHeader("Content-Type", "application/json");
-
-            AuthDTO authRequest = AuthDTO.builder()
-                    .username(username)
-                    .password(password)
-                    .type("normal")
-                    .build();
-
-            String jsonBody = objectMapper.writeValueAsString(authRequest);
-
-            StringEntity entity = new StringEntity(jsonBody, StandardCharsets.UTF_8);
-            post.setEntity(entity);
-
-            HttpResponse response = httpClient.execute(post);
-
-            if (response.getStatusLine().getStatusCode() != 200) {
-                throw new RuntimeException("Falha na autenticação: " + response.getStatusLine().getStatusCode());
-            }
-
-            String responseString = EntityUtils.toString(response.getEntity());
-            JsonNode jsonNode = objectMapper.readTree(responseString);
-
-            return jsonNode.get("auth_token").asText();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao autenticar no Taiga", e);
-        }
-    }
 }
