0e21e43d79a50eed5260bf140b9e22cbacc4b310
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-14 22:18:21 -0300

    🧪(test): fetching taiga data

--- src/test/java/com/manolito/dashflow/service/dw/TaigaServiceTest.java
+++ src/test/java/com/manolito/dashflow/service/dw/TaigaServiceTest.java
@@ -1,13 +1,22 @@
 package com.manolito.dashflow.service.dw;
 
+import com.manolito.dashflow.util.SparkUtils;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@RequiredArgsConstructor
 class TaigaServiceTest {
+
+    private final SparkUtils utils;
 
     @Autowired
     private TaigaService taigaService;
@@ -18,5 +27,26 @@ class TaigaServiceTest {
         String password = "aluno123";
         String authToken = taigaService.authenticateTaiga(username, password);
         assertNotNull(authToken, "O token de autenticação não deve ser nulo");
+    }
+
+    @Test
+    void testFetchTaigaDataWithMocks() {
+        when(taigaService.handleProjects()).thenReturn(mockDataset());
+
+        Dataset<Row> projects = taigaService.handleProjects();
+        assertNotNull(projects, "A lista de projetos não deve ser nula");
+
+        Dataset<Row> userStories = taigaService.handleUserStories();
+        assertNotNull(userStories, "A lista de user stories não deve ser nula");
+
+        Dataset<Row> tasks = taigaService.handleTasks();
+        assertNotNull(tasks, "A lista de tasks não deve ser nula");
+
+        Dataset<Row> issues = taigaService.handleIssues();
+        assertNotNull(issues, "A lista de issues não deve ser nula");
+    }
+
+    private Dataset<Row> mockDataset() {
+        return mock(Dataset.class);
     }
 }
