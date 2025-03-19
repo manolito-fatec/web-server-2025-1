e028d56f583045917ddb1fe98b7511c5cb9febd5
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-18 21:11:15 -0300

    🔥(chore): remove TaigaService tests
    TaigaServices tests weren't useful in curent context

--- src/test/java/com/manolito/dashflow/service/dw/TaigaServiceTest.java
+++ src/test/java/com/manolito/dashflow/service/dw/TaigaServiceTest.java
@@ -15,36 +15,7 @@ class TaigaServiceTest {
 
     @Autowired
     private TaigaService taigaService;
-
-    @Test
-    void testAuthenticateTaiga() {
-        String username = "gabguska";
-        String password = "aluno123";
-//        String authToken = taigaService.authenticateTaiga(username, password);
-//        assertNotNull(authToken, "O token de autenticação não deve ser nulo");
-    }
-
-    @Test
-    void testFetchTaigaDataWithMocks() {
-        when(taigaService.handleProjects()).thenReturn(mockDataset());
-
-        Dataset<Row> projects = taigaService.handleProjects();
-        assertNotNull(projects, "A lista de projetos não deve ser nula");
-
-        Dataset<Row> userStories = taigaService.handleUserStories();
-        assertNotNull(userStories, "A lista de user stories não deve ser nula");
-
-        Dataset<Row> tasks = taigaService.handleTasks();
-        assertNotNull(tasks, "A lista de tasks não deve ser nula");
-
-        Dataset<Row> issues = taigaService.handleIssues();
-        assertNotNull(issues, "A lista de issues não deve ser nula");
-    }
-
-    private Dataset<Row> mockDataset() {
-        return mock(Dataset.class);
-    }
-
+    
     @Test
     void testAutheticateUserThenSaveInDatabase() {
         taigaService.authenticateTaiga("gabguska", "aluno123");
