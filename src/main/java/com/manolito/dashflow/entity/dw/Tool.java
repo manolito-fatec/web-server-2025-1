9b26eec67791cb8b6a72ea2f279c74ce9ed472bd
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-20 12:08:06 -0300

    ✨(feat): Tool entity

---
+++ src/main/java/com/manolito/dashflow/entity/dw/Tool.java
@@ -0,0 +1,43 @@
+package com.manolito.dashflow.entity.dw;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tools", schema = "dw_tasks")
+public class Tool {
+    @Id
+    @Column(name = "tool_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "seq", nullable = false)
+    private Integer seq;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "tool_name", nullable = false)
+    private String toolName;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @NotNull
+    @Column(name = "is_current", nullable = false)
+    private Boolean isCurrent = false;
+
+}
