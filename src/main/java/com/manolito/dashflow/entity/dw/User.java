745d267086dee61425fd40a367604088158b313a
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-20 12:08:30 -0300

    ✨(feat): User entity

---
+++ src/main/java/com/manolito/dashflow/entity/dw/User.java
@@ -0,0 +1,62 @@
+package com.manolito.dashflow.entity.dw;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users", schema = "dw_tasks")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_gen")
+    @SequenceGenerator(name = "users_id_gen", sequenceName = "users_user_id_seq", allocationSize = 1)
+    @Column(name = "user_id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "seq", nullable = false)
+    private Integer seq;
+
+    @NotNull
+    @Column(name = "original_id", nullable = false)
+    @Type(type = "org.hibernate.type.TextType")
+    private String originalId;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tool_id", nullable = false)
+    private Tool tool;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "description")
+    @Type(type = "org.hibernate.type.TextType")
+    private String description;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @NotNull
+    @Column(name = "is_current", nullable = false)
+    private Boolean isCurrent = false;
+
+}
