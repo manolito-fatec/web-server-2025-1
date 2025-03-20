433177cc07ac95f522ba7c09aba8813a66a6f7f1
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-20 12:07:08 -0300

    🐛(fix): UserRepository missing Jpa

--- src/main/java/com/manolito/dashflow/repository/dw/UserRepository.java
+++ src/main/java/com/manolito/dashflow/repository/dw/UserRepository.java
@@ -1,6 +1,13 @@
 package com.manolito.dashflow.repository.dw;
 
-public interface UserRepository {
+import com.manolito.dashflow.entity.dw.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-    Long getUserIdByOriginalId(String originalId);
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u.id FROM User u WHERE u.originalId = :originalId")
+    Long getUserIdByOriginalId(@Param("originalId") String originalId);
+
 }
