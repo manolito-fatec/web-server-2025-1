46bf2aeb2c345f96e158f111acaaaca64f4ba2dc
André Wakugawa <117671087+AndreWakugawa@users.noreply.github.com>
2025-03-21 14:51:06 -0300

    ✨(feat): tasks service with initial get methods
    service created with:
    
    getTaskCountByOperatorId;
    getTaskCountByOperatorIdBetween.

---
+++ src/main/java/com/manolito/dashflow/service/application/TasksService.java
@@ -0,0 +1,32 @@
+package com.manolito.dashflow.service.application;
+
+import com.manolito.dashflow.repository.application.TasksDataWarehouseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import scala.Int;
+
+import java.time.LocalDate;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TasksService {
+    private final TasksDataWarehouseRepository tasksDataWarehouseRepository;
+
+    Integer getTaskCountByOperatorId(Integer userId) {
+        Optional<Integer> taskCount = tasksDataWarehouseRepository.getTotalTasksByOperator(userId);
+        if (taskCount.isEmpty()) {
+            throw new NoSuchElementException("No tasks found");
+        }
+        return taskCount.get();
+    }
+
+    Integer getTaskCountByOperatorIdBetween(Integer userId, LocalDate startDate, LocalDate endDate) {
+        Optional<Integer> taskCount = tasksDataWarehouseRepository.getTotalTasksByOperatorBetween(userId, startDate, endDate);
+        if (taskCount.isEmpty()) {
+            throw new NoSuchElementException("No tasks found in the time period");
+        }
+        return taskCount.get();
+    }
+}
