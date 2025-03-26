bb32189bd06beddf1afadca3c8eaa339d1e74576
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-03-25 22:01:33 -0300

    ✨(feat): Averege Completion Time service with initial get methods

---
+++ src/main/java/com/manolito/dashflow/service/application/AveregeTimeService.java
@@ -0,0 +1,22 @@
+package com.manolito.dashflow.service.application;
+
+import com.manolito.dashflow.repository.application.AveregeTimeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AveregeTimeService {
+    private final AveregeTimeRepository averegeTimeRepository;
+
+    public Long getAveregeTimeCard() {
+        Optional<Long> averegeTimeCard = averegeTimeRepository.getAveregeTimeCard();
+        if (averegeTimeCard.isPresent()) {
+            return averegeTimeCard.get();
+        }
+        throw new NoSuchElementException("No tasks completed");
+    }
+}
