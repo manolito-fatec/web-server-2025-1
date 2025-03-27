c1daee3c0edb9598f9fb59d1b277be61850d17d2
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-27 16:42:13 -0300

    ⚰️(chore): removed unused annotation

--- src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
+++ src/main/java/com/manolito/dashflow/service/dw/TaigaService.java
@@ -26,7 +26,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.manolito.dashflow.enums.ProjectManagementTool.TAIGA;
@@ -210,8 +209,7 @@ public class TaigaService {
                         pair -> pair[0]
                 ));
     };
-
-    //@PostConstruct
+    
     public Dataset<Row> saveUserRoleToDatabase() {
         final long projectId = 1637322L;
 
