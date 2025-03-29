3bc7ef1def632223db94becf4630ccfd958d6052
Beatriz <beatriz.bonatto@fatec.sp.gov.br>
2025-03-28 23:38:51 -0300

    📝 (docs): add JavaDoc to service method getAverageTimeCard

--- src/main/java/com/manolito/dashflow/service/application/TasksService.java
+++ src/main/java/com/manolito/dashflow/service/application/TasksService.java
@@ -29,6 +29,13 @@ public class TasksService {
         return taskCount.get();
     }
 
+    /**
+     * Busca a média de tempo que o usuário leva para completar suas tasks, calculando a média de tasks feitas por semana.
+     * @param userId O id do usuário buscado para o cálculo.
+     * @return valor da média calculada, em formato '0.0'
+     * @throws 'No tasks completed'
+     */
+
     public Double getAverageTimeCard(Integer userId) {
         Optional<Double> averageTimeCard = tasksDataWarehouseRepository.getAverageTimeCard(userId);
         if (averageTimeCard.isPresent()) {
