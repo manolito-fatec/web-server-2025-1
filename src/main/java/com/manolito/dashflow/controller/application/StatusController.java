5a36b08fa31d23cddcf8a79c3ada4d66236daf64
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-03-29 13:38:18 -0300

    🐛(fix): missing cors in StatusController

--- src/main/java/com/manolito/dashflow/controller/application/StatusController.java
+++ src/main/java/com/manolito/dashflow/controller/application/StatusController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.NoSuchElementException;
 
 @Tag(name = "Status Controller", description = "Endpoints para a consulta de status no Data Warehouse de Tasks")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/status")
 @RequiredArgsConstructor
 public class StatusController {
