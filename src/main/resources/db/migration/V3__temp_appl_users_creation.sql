eaddee71dcca252f5984f79bb823115316d15c20
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-04-08 22:56:56 -0300

    ♻️ (refactor): renamed application schema

--- src/main/resources/db/migration/V3__temp_appl_users_creation.sql
+++ src/main/resources/db/migration/V3__temp_appl_users_creation.sql
@@ -2,35 +2,35 @@ INSERT INTO dataflow_appl.tools(
 	tool_id, tool_name)
 	VALUES (1, 'taiga');
 
-INSERT INTO dataflow_appl.users (username, password, email)
+INSERT INTO dashflow_appl.users (username, password, email)
 VALUES (
     'Andre',
 	'andre',
     'andre.andre@andre.com'
 );
 
-INSERT INTO dataflow_appl.users (username, password, email)
+INSERT INTO dashflow_appl.users (username, password, email)
 VALUES (
     'Bia',
 	'bia',
     'bia.bia@bia.com'
 );
 
-INSERT INTO dataflow_appl.users (username, password, email)
+INSERT INTO dashflow_appl.users (username, password, email)
 VALUES (
     'Caue',
 	'caue',
     'caue.caue@caue.com'
 );
 
-INSERT INTO dataflow_appl.accounts(
+INSERT INTO dashflow_appl.accounts(
 	account, user_id, tool_id)
 	VALUES ('755290', 1, 1);
 
-INSERT INTO dataflow_appl.accounts(
+INSERT INTO dashflow_appl.accounts(
 	account, user_id, tool_id)
 	VALUES ('758256', 2, 1);
 
-INSERT INTO dataflow_appl.accounts(
+INSERT INTO dashflow_appl.accounts(
 	account, user_id, tool_id)
 	VALUES ('754575', 3, 1);
