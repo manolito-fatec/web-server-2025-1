afe39914510f3caafdfb9a30f098ecd5965a987f
pauloarantesmahado <pauloantoniomachado@hotmail.com>
2025-05-10 21:02:19 -0300

    :sparkles: (feat): Added new attributes to the SignupRequestDto: toolUserId and toolId.

--- src/main/java/com/manolito/dashflow/dto/application/auth/SignupRequestDto.java
+++ src/main/java/com/manolito/dashflow/dto/application/auth/SignupRequestDto.java
@@ -10,7 +10,10 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignupRequestDto {
+    private String username;
     private String email;
     private String password;
     private Set<String> roles;
+    private String toolUserId;
+    private Integer toolId;
 }
