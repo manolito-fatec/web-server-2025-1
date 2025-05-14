8d86843b61317ec1f688703fa82cfc36079c1524
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-05-13 21:07:28 -0300

    ✨(feat): initial setup to JiraService

---
+++ src/main/java/com/manolito/dashflow/service/dw/JiraService.java
@@ -0,0 +1,47 @@
+package com.manolito.dashflow.service.dw;
+
+import com.manolito.dashflow.loader.TasksDataWarehouseLoader;
+import com.manolito.dashflow.util.SparkUtils;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+import static com.manolito.dashflow.enums.ProjectManagementTool.JIRA;
+
+@Service
+@RequiredArgsConstructor
+public class JiraService {
+
+    private final SparkSession spark;
+    private final SparkUtils utils;
+    private final TasksDataWarehouseLoader dataWarehouseLoader;
+
+    private String mapNameField(String tableName) {
+        return switch (tableName) {
+            case "projects" -> "project_name";
+            case "status" -> "status_name";
+            case "users" -> "user_name";
+            case "tags" -> "tag_name";
+            default -> throw new IllegalArgumentException("Unsupported table for name field mapping: " + tableName);
+        };
+    }
+
+    private Dataset<Row> fetchAndConvertToDataFrame(String endpoint, String tableName) {
+        String jsonResponse = utils.fetchDataFromEndpointTrello(JIRA.getBaseUrl() + endpoint);
+        Dataset<org.apache.spark.sql.Row> data = utils.fetchDataAsDataFrame(jsonResponse);
+
+        data = data.withColumn("tool_id", functions.lit(2));
+
+        String mappedNameColumn = mapNameField(tableName);
+        if (Arrays.asList(data.columns()).contains("name")) {
+            data = data.withColumnRenamed("name", mappedNameColumn);
+        }
+
+        return data;
+    }
+}
