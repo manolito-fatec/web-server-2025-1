4fba730bb2e2f94871b9aaa76295b684f7e11565
GabrielBartolomeu1 <127538473+GabrielBartolomeu1@users.noreply.github.com>
2025-05-13 21:06:26 -0300

    ✨(feat): add JiraAuthDto

---
+++ src/main/java/com/manolito/dashflow/dto/dw/JiraAuthDto.java
@@ -0,0 +1,11 @@
+package com.manolito.dashflow.dto.dw;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class JiraAuthDto {
+    private String email;
+    private String apiToken;
+}
