c5e7544230aea26d8d5e8aa56f0ec1e534c03cb7
André Wakugawa <andre.wakugawa@outlook.com>
2025-05-23 20:30:53 -0300

    ✨(feat): filtered issue repo query handling
    Comments added to explain the query appends on optional filters;
    
    replaced String with StringBuilder  for better performance on concatenations;

--- src/main/java/com/manolito/dashflow/repository/application/IssuesDataWarehouseRepository.java
+++ src/main/java/com/manolito/dashflow/repository/application/IssuesDataWarehouseRepository.java
@@ -1,8 +1,10 @@
 package com.manolito.dashflow.repository.application;
 
 import com.manolito.dashflow.dto.dw.IssueCountDto;
+import com.manolito.dashflow.dto.dw.IssueFilterRequestDto;
+import com.manolito.dashflow.enums.IssuePriority;
+import com.manolito.dashflow.enums.IssueSeverity;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -46,34 +48,50 @@ public class IssuesDataWarehouseRepository {
         return result;
     }
 
-    public Optional<Integer> getIssueCountByType(String projectOriginalId, String severity, String priority, String type) {
-        String sql = """
-                SELECT COUNT(fi.issue_id) AS issue_count
-                FROM dw_dashflow.fact_issues fi
-                JOIN dw_dashflow.projects proj ON fi.project_id = proj.project_id
-                JOIN dw_dashflow.issue_severity sev ON fi.severity_id = sev.severity_id
-                JOIN dw_dashflow.issue_priority pri ON fi.priority_id = pri.priority_id
-                JOIN dw_dashflow.issue_type typ ON fi.type_id = typ.type_id
-                WHERE proj.original_id = :projectOriginalId
-                AND sev.severity_name = :severity
-                AND pri.priority_name = :priority
-                AND typ.type_name = :type
-                AND sev.is_current = TRUE
-                AND pri.is_current = TRUE
-                AND typ.is_current = TRUE
-                """;
+    public List<IssueCountDto> getIssueCountsByFilter(IssueFilterRequestDto filter) {
+        // Base query without optional filters
+        StringBuilder sql = new StringBuilder("""
+        SELECT typ.type_name AS type, COUNT(fi.issue_id) AS count
+        FROM dw_dashflow.fact_issues fi
+        JOIN dw_dashflow.projects proj ON fi.project_id = proj.project_id
+        JOIN dw_dashflow.issue_severity sev ON fi.severity_id = sev.severity_id
+        JOIN dw_dashflow.issue_priority pri ON fi.priority_id = pri.priority_id
+        JOIN dw_dashflow.issue_type typ ON fi.type_id = typ.type_id
+        WHERE proj.original_id = :projectId
+        AND sev.is_current = TRUE
+        AND pri.is_current = TRUE
+        AND typ.is_current = TRUE
+        """);
 
         Map<String, Object> params = new HashMap<>();
-        params.put("projectOriginalId", projectOriginalId);
-        params.put("severity", severity);
-        params.put("priority", priority);
-        params.put("type", type);
+        params.put("projectId", filter.getProjectId());
 
-        try {
-            Integer result = jdbcTemplate.queryForObject(sql, params, Integer.class);
-            return Optional.ofNullable(result);
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
+        // Handle severities List to append to the WHERE filter if not null
+        if (filter.getSeverities() != null && !filter.getSeverities().isEmpty()) {
+            sql.append(" AND sev.severity_name IN (:severities)");
+            params.put("severities", filter.getSeverities().stream()
+                    .map(IssueSeverity::getValue)
+                    .collect(Collectors.toList()));
         }
+
+        // Handle priorities List to append to the WHERE filter if not null
+        if (filter.getPriorities() != null && !filter.getPriorities().isEmpty()) {
+            sql.append(" AND pri.priority_name IN (:priorities)");
+            params.put("priorities", filter.getPriorities().stream()
+                    .map(IssuePriority::getValue)
+                    .collect(Collectors.toList()));
+        }
+
+        // Finally adds the GROUP BY clause with or without the severities and priorities filters
+        sql.append(" GROUP BY typ.type_name");
+
+        return jdbcTemplate.query(
+                sql.toString(),
+                params,
+                (rs, rowNum) -> IssueCountDto.builder()
+                        .count(rs.getInt("count"))
+                        .type(rs.getString("type"))
+                        .build()
+        );
     }
 }
